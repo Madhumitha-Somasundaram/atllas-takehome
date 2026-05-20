012f1a356125ac6aefee367c0ecb24f22af4baf4
Madhumitha-Somasundaram <madhusomasundaram17@gmail.com>
2026-05-20 14:39:34 -0600

    adding users type file

---
+++ packages/front-end/src/types/user.ts
@@ -0,0 +1,11 @@
+export type User = {
+  id: number;
+  registered: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  email: string;
+  phoneNumber?: string;
+  address?: string;
+  adminNotes?: string;
+};
