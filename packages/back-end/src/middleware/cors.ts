4786f14d9090360f6e8245f352d06dd8efc31c7e
Madhumitha-Somasundaram <madhusomasundaram17@gmail.com>
2026-05-20 15:36:15 -0600

    added cors

--- packages/back-end/src/middleware/cors.ts
+++ packages/back-end/src/middleware/cors.ts
@@ -2,6 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 
 export default function CorsStar(req: Request, res: Response, next: NextFunction) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,DELETE,OPTIONS'
+  );
 
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 }
